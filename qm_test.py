4f838f6310bad6b1a127def30a5f7e82b5f67955
hzseu71 <zhuu0055@student.monash.edu>
2025-05-29 15:38:33 +0800

    add rho

--- qm_test.py
+++ qm_test.py
@@ -94,18 +94,26 @@ for name, (rho, mu_xlsx, raw_path) in MATERIALS.items():
 
 print("Loaded materials:", list(MATERIALS))
 
+TARGET_FACTOR = 0.97        # mgfpj primary ×0.97 → mcgpu
+BONE_THICK_REF = 3.0        # ← 测 0.97 时那张纯骨像素的厚度(cm)
+ln_factor = -np.log(TARGET_FACTOR)          # >0
+delta_mu = ln_factor / BONE_THICK_REF       # 标量 (cm-1)
+
+# delta_mu_E = delta_mu * energy_weight[k] ...
 
 # 3. 计算 lower 与 higher
 
 lower  = np.sum(spec_w * spec_e)             # 入射总能量 (标量),用numpy来加速积分过程
 higher = np.zeros(RAW_SHAPE, dtype=np.float32)
 for k, E_keV in enumerate(ENERGY_GRID):
-    # 3-1 先算总线性衰减 μ·d (H×W)
     mu_t = np.zeros(RAW_SHAPE, dtype=np.float32)
+
     for name, (rho, _, _) in MATERIALS.items():
-        mu   = mu_dict[name][k]              # 此能量点的 μ/ρ
-        thick= thick_dict[name]              # mm
-        mu_t += mu * rho * thick * MM2CM     # μ/ρ × ρ × t(cm)
+        mu = mu_dict[name][k]            # (μ/ρ)_E
+        thick = thick_dict[name]         # mm
+        if name == "bone":
+            mu += delta_mu / rho         # ← 只给骨加 Δμ/ρ
+        mu_t += mu * rho * thick * MM2CM
     # 3-2 能量加权累加
     higher += spec_w[k] * spec_e[k] * np.exp(-mu_t)
 
@@ -113,78 +121,6 @@ for k, E_keV in enumerate(ENERGY_GRID):
 # 4. 取透过率、Post-log，并写 raw
 primary  = higher / lower
 
-
-'''
-======================================
-part 2 散射信号叠加
-======================================
-'''
-#
-# # ── 0. 基本参数 ────────────────────────────────────────────────
-# dtype1         = np.float32
-# SCAT_SHAPE     = (300, 300)                     # (H, W) 本项目仅一张图,没有多余切片
-# USE_AIR_FLAT   = False                          # 没有 AIR-flat 就设 False(对空气图做多张合成)
-# sigma_px       = 5                             # 高斯核
-#
-# # ── 1. 读散射帧（取第 3 个切片）并左旋 90° ────────────────────
-# nz = 1
-# H, W  =SCAT_SHAPE          # 720 × 300 × 300
-# scatter   = np.zeros(SCAT_SHAPE, dtype=dtype1)
-#
-# for i in range(nz):
-#     fname = f"{scatter_prefix}.raw"
-#     path  = os.path.join(scatter_folder, fname)
-#
-#     # ① 读取 (H, W, 3) → 取索引 2 作为散射层
-#     slice3 = imreadRaw(path, H, W, nSlice=3, dtype=dtype1)[2]
-#
-#     # ② 左旋 90°（逆时针）——等价于 np.rot90(slice3, 1)
-#     slice3 = cv2.rotate(slice3, cv2.ROTATE_90_COUNTERCLOCKWISE)
-#     # 若不用 OpenCV，可用： slice3 = np.rot90(slice3, 1)
-#     imwriteRaw(slice3, out_dir_test / f"scatter_noGB_test.raw", dtype=dtype1)  # this code for debug
-#     # ③ 可选：高斯平滑（低频化散射）
-#     slice3 = cv2.GaussianBlur(slice3, (75,75), sigma_px)
-#     imwriteRaw(slice3, out_dir_test / f"scatter_GB_test.raw", dtype=dtype1)  # this code for debug
-#
-#
-#     scatter = slice3
-#
-# # ── 2. 读 AIR-flat 并做归一化 (当空气图光子量过低时用) ──────────────────────────
-# if USE_AIR_FLAT:
-#     air_vol = []
-#     for f in sorted(os.listdir(AIR_PATH)):
-#         if f.endswith(".raw"):
-#             air_vol.append(imreadRaw(os.path.join(AIR_PATH, f), H, W, nSlice=1,
-#                                      dtype=dtype1).squeeze())
-#     mean_air = np.mean(np.stack(air_vol), axis=0) + 1e-6      # 防除零
-#     scatter /= mean_air                                       # element-wise
-#     del air_vol
-# else:           # 使用单张空气图来完成逻辑
-#     # 1) 读取单张 air：nSlice=3 取第1个切片,即带散射
-#     air_img = imreadRaw(AIR_PATH, H, W, nSlice=3,
-#                         dtype=dtype1)[0]
-#
-#     # 2) 做同样的几何处理——左旋 90°，保持坐标对应
-#     air_img = cv2.rotate(air_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-#     imwriteRaw(air_img, out_dir_test / f"Air_noGB_test.raw", dtype=dtype1)  # this code for debug
-#     air_img = cv2.GaussianBlur(air_img, (75, 75), sigma_px)
-#     imwriteRaw(air_img, out_dir_test / f"Air_GB_test.raw", dtype=dtype1)  # this code for debug
-#
-#     # 3) 防除零；把散射归一化到 I/I0 量纲
-#     air_img += 1e-6
-#     scatter /= air_img
-#     imwriteRaw(scatter, out_dir_test / f"scatter_ii0_test.raw", dtype=dtype1)  # this code for debug
-#
-# # out_dir = Path("./proj_with_scatter")
-# # out_dir.mkdir(exist_ok=True, parents=True)
-# # imwriteRaw(scatter, out_dir / f"test4.raw", dtype=dtype1)  # this code for debug
-#
-#
-# # ── 3. 合并到 primary 并做 post-log ───────────────────────────
-# # primary 在前面得到
-# assert primary.shape == scatter.shape, "primary / scatter 尺寸不一致！"
-#
-# total   = -np.log(primary + scatter + 1e-6)      # 加小常数防 log0
 postlog  = -np.log(primary + 1e-7)  # 对primary做postlog,已方便对比
 # ── 4. 输出 ────────────────────────────────────────────────────
 out_dir = Path("./proj_with_scatter")
