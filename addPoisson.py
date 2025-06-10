7954cd48ab49d6c7142306bb8081de9cde147ed1
hzseu71 <zhuu0055@student.monash.edu>
2025-06-10 20:51:53 +0800

    modify_6102

--- addPoisson.py
+++ addPoisson.py
@@ -7,18 +7,18 @@ from crip.io import imreadRaw, imwriteRaw  # 可替换成 numpy 自定义函数
 H, W = 300, 300                 # 图像尺寸
 dtype = np.float32
 photon_in = 1e6               # 入射光子数
-gaussian_sigma = 5           # 高斯滤波标准差
+gaussian_sigma =  2          # 高斯滤波标准差
 
 # === 路径设置 ===
 primary_path       = Path("./proj_with_scatter/P30_primary_muti_100kv_by_QM_64_2.raw")
-scatter_path       = Path("./mcgpu/scat_raw/P30_muti_610_a.raw")
+scatter_path       = Path("./mcgpu/scat_raw/P30_muti_610_b.raw")
 air_primary_path   = Path("./proj_with_scatter/Air_primary_muti_100kv_by_QM_67_1.raw")
 air_scatter_path   = Path("./mcgpu/scat_raw/air/P30_muti_100kv_repeat_1_Air.raw")
 
-output_poisson_path = Path("./poisson/output_poisson_G5_H1e6.raw")
-output_postlog_path = Path("./poisson/output_postlog_poisson_G5_H1e6.raw")
-output_scatter_path = Path("./poisson/output_scatter_G5_H1e6.raw")
-output_t_path = Path("./poisson/output_t_8.raw")
+output_poisson_path = Path("./poisson/output_poisson_G2_H0e6_c.raw")
+output_postlog_path = Path("./poisson/output_postlog_poisson_G2_H0e6_c.raw")
+output_scatter_path = Path("./poisson/output_scatter_G2_H0e6_c.raw")
+output_t_path = Path("./poisson/output_t_610_c.raw")
 # === 读入图像 ===
 primary          = imreadRaw(str(primary_path), H, W, nSlice=1, dtype=dtype).squeeze()
 scatter_full     = imreadRaw(str(scatter_path), H, W, nSlice=3, dtype=dtype).squeeze()
@@ -30,14 +30,17 @@ scatter      = np.rot90(scatter, k=1)          # 左旋90°
 air_scatter  = air_scatter_full[0]
 
 # === 高斯滤波 ===
-scatter_filtered = gaussian_filter(scatter, sigma=gaussian_sigma)
+# scatter_filtered = gaussian_filter(scatter, sigma=gaussian_sigma)
+scatter_filtered = scatter
 imwriteRaw(scatter_filtered,str(output_scatter_path), dtype=dtype)
 # === 步骤1：归一化 Primary 与 Scatter 图像 ===
 norm_primary = primary / (air_primary + 1e-6)
 norm_scatter = scatter_filtered / (air_scatter + 1e-6)
-
+imwriteRaw(norm_primary,"./poisson/norm_primary.raw", dtype=dtype)
+imwriteRaw(norm_scatter,"./poisson/norm_scatter.raw", dtype=dtype)
 # === 步骤2：计算透过光子数 ===
 total_transmission = (norm_primary + norm_scatter) * photon_in
+# total_transmission = (norm_primary) * photon_in
 imwriteRaw(total_transmission,str(output_t_path), dtype=dtype)
 # === 步骤3：加入泊松噪声 ===
 poisson_noisy = np.random.poisson(total_transmission).astype(dtype)
--- fpj/runFPJ.py
+++ fpj/runFPJ.py
@@ -2,9 +2,9 @@ import os
 from crip.io import imreadRaw,imwriteRaw
 import numpy as np
 
-from run_mgfpj_v3 import *
-import cv2
-run_mgfpj_v3("./mgfpj_mcgpu3.jsonc")
+# from run_mgfpj_v3 import *
+# import cv2
+# run_mgfpj_v3("./mgfpj_mcgpu3.jsonc")
 
 from run_mgfbp import *
-run_mgfbp("./mgfpj_mcgpu3.jsonc")
+run_mgfbp("./abc/config_mgfbp_realHead.jsonc")
